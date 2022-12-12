468e5dc8913d36cc04a6ee05c95bf0af2457ad7b
mgk5543 <mgk5543@nyu.edu>
2022-12-12 17:39:10 -0500

    Got image data from URL

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
 
 function Post({ caption, imageAlt, imageUrl, userName, userId }) {
+    const [image, setImage] = useState();
+    useEffect(() => {
+        const storage = getStorage();
+        getDownloadURL(ref(storage,'images/' + imageUrl))
+        .then((url) => {
+            setImage(url);
+        });
+        
+    },[imageUrl]);
+    if (image == null){
+
+    }
+    else{
     return (
-        <div className="PostWrapper">
+        <div className="PostComponentWrapper">
             <img 
-            src={imageUrl} 
+            className="PostComponent--image"
+            src={image} 
             alt={imageAlt}
              />
             <p className="Caption">{caption}</p>
             <p>Posted by: <a href={`user/${userId}`}>{userName}</a></p>
         </div>
     )
+    }
 }
 
 export default Post;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,9 @@
 import { React, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
+import { Link } from "react-router-dom";
+
 import { getDocs, getFirestore, collection } from "firebase/firestore";
+import { getStorage,ref } from 'firebase/storage';
 import Header from '../components/Header';
 import Post from '../components/Post'
 import { async } from '@firebase/util';
@@ -33,16 +36,39 @@ function HomePage({ app, isLoading, isLoggedIn, setIsLoggedIn, setUserInformatio
         <>
             <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUserInformation={setUserInformation} />
             <div className="PageWrapper">
-                <h1>Home Page</h1>
-                {postData.map((post) => (
-                    <Post
-                        caption={post.caption}
-                        imageAlt={post.imageAlt}
-                        imageUrl={post.imageUrl}
-                        userId={post.userId}
-                        userName={post.userName}
-                    />
-                ))}
+                <div className="cover">
+                    <div className="cover--left">
+                        <h1>brandspiration</h1>
+                        <h3>Simple branding templates to start off your designs!</h3>
+                    </div>
+                    <div className="cover--right">
+                        <h2 style={{color: "#D8AC90"}}>1 IMAGE!</h2>
+                        <h2 style={{color: "#D83D66"}}>2 FONTS!</h2>
+                        <h2 style={{color: "#15183B"}}>3 COLORS!</h2>
+                    </div>
+                </div>
+                {isLoggedIn && (
+                    <li className="create-post--home">
+                        <Link to="/create-post"><p>Create your own</p></Link>
+                    </li>
+                )}
+                {!isLoggedIn && (
+                    <li className="create-post--home">
+                        <Link to="/login"><p>Create your own</p></Link>
+                    </li>
+                )}
+                <a className="explore--home" href="#scroll-to">Explore</a>
+                <section id="scroll-to">
+                    {postData.map((post) => (
+                        <Post
+                            caption={post.caption}
+                            imageAlt={post.imageAlt}
+                            imageUrl={post.imageUrl}
+                            userId={post.userId}
+                            userName={post.userName}
+                        />
+                    ))}
+                </section>
             </div>
         </>
     );
